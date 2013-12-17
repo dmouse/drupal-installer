0753c0460278b998112ab6e5e49ace67db402a7a
David Flores <dmousex@gmail.com>
2013-12-16 23:07:42 -0600

    create TemplateInstaller

--- src/HechoEnDrupal/Composer/TemplateInstallerPlugin.php
+++ src/HechoEnDrupal/Composer/TemplateInstallerPlugin.php
@@ -3,42 +3,13 @@ namespace HechoEnDrupal\Composer;
 
 use Composer\Package\PackageInterface;
 use Composer\Installer\LibraryInstaller;
+use Composer\Plugin\PluginInterface;
 
-class TemplateInstallerPlugin extends LibraryInstaller {
-  /**
-   * Initializes base installer.
-   *
-   * @param PackageInterface $package
-   * @param Composer         $composer
-   */
-  public function __construct(PackageInterface $package = null, Composer $composer = null) {
-    $this->composer = $composer;
-    $this->package = $package;
-  }
+class TemplateInstallerPlugin implements PluginInterface {
 
   public function activate(Composer $composer, IOInterface $io){
-
+    $installer = new TemplateInstaller($io, $composer);
+    $composer->getInstallationManager()->addInstaller($installer);
   }
 
-  /**
-   * {@inheritDoc}
-   */
-  public function getPackageBasePath(PackageInterface $package) {
-    $prefix = substr($package->getPrettyName(), 0, 23);
-    if ('phpdocumentor/template-' !== $prefix) {
-      throw new \InvalidArgumentException(
-        'Unable to install template, phpdocumentor templates '
-        .'should always start their package name with '
-        .'"phpdocumentor/template-"'
-      );
-    }
-    return 'data/templates/'.substr($package->getPrettyName(), 23);
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  public function supports($packageType){
-    return 'drupal-template' === $packageType;
-  }
 }
